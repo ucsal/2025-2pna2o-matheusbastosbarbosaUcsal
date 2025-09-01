900b5e0658ac46128b5463a7a9c86de0441b0dda
matheusbastosbarbosaUcsal <matheusbastos.barbosa@ucsal.edu.br>
2025-09-01 12:17:05 -0300

    Modificações feitas e ajustadas

--- src/test/java/br/com/mariojp/solid/ocp/DiscountCalculatorTest.java
+++ src/test/java/br/com/mariojp/solid/ocp/DiscountCalculatorTest.java
@@ -1,12 +1,21 @@
 package br.com.mariojp.solid.ocp;
 
 import org.junit.jupiter.api.Test;
+import java.util.Map;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DiscountCalculatorTest {
+
     @Test
     void partner_gets_12_percent() {
-        var calc = new DiscountCalculator();
+            Map<CustomerType, DiscountPolicy> policies = Map.of(
+            CustomerType.REGULAR, new RegularPolicy(),
+            CustomerType.PREMIUM, new PremiumPolicy(),
+            CustomerType.PARTNER, new PartnerPolicy()
+        );
+        
+        var calc = new DiscountCalculator(policies);
+
         assertEquals(88.0, calc.apply(100.0, CustomerType.PARTNER), 0.0001,
             "PARTNER deveria ter 12% de desconto");
     }
